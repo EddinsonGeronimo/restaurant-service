874c1e1fbc0098c5a49cabbb7f58dc7466fd31ff
simulated_reality <lizardogeronimo24@gmail.com>
2021-06-16 19:17:35 -0400

    external url test

--- api/main.go
+++ api/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	_ "encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
@@ -13,5 +17,26 @@ func main() {
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("welcome"))
 	})
+
+	// Load data to local database
+	r.Get("/customers",func(w http.ResponseWriter, r *http.Request) {
+		
+		currentTime := time.Now().Format("2006-01-02")
+
+		urlCustomers := fmt.Sprintf("https://kqxty15mpg.execute-api.us-east-1.amazonaws.com/buyers?date=%s", currentTime)
+
+		resp, err := http.Get(urlCustomers)
+		
+		if err != nil {
+			fmt.Printf("error: %s\n", err.Error())
+		}
+		
+		defer resp.Body.Close()
+		
+		body, err := ioutil.ReadAll(resp.Body)
+
+		w.Write(body)
+	})
+
 	http.ListenAndServe(":3000", r)
 }
