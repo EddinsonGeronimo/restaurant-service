4b37ba2a7901298a8175aba63299aa91e3572361
simulated_reality <lizardogeronimo24@gmail.com>
2021-06-17 08:48:57 -0400

    load test data

---
+++ api/go.mod
@@ -0,0 +1,12 @@
+module github.com/EddinsonGeronimo/restaurant-service
+
+go 1.15
+
+require (
+	github.com/dgraph-io/dgo/v200 v200.0.0-20210401091508-95bfd74de60e // indirect
+	github.com/dgraph-io/dgo/v210 v210.0.0-20210421093152-78a2fece3ebd // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
+	golang.org/x/sys v0.0.0-20210616094352-59db8d763f22 // indirect
+	google.golang.org/genproto v0.0.0-20210614182748-5b3b54cad159 // indirect
+)
--- api/main.go
+++ api/main.go
@@ -1,44 +1,113 @@
 package main
 
 import (
-	_ "encoding/json"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"time"
+	"log"
+	"context"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
-	_"github.com/dgraph-io/dgo"
-	_"github.com/dgraph-io/dgo/protos/api"
-	_"google.golang.org/grpc"
+	"github.com/dgraph-io/dgo"
+	"github.com/dgraph-io/dgo/protos/api"
+	"google.golang.org/grpc"
 )
 
+type Person struct {
+	Uid  string `json:"uid,omitempty"`
+	Name string `json:"name,omitempty"`
+}
+
 func main() {
+	
+	/*
+	* chi router
+	*/
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("welcome"))
 	})
 
-	// Load data to local database
+	// load data to local database
 	r.Get("/customers",func(w http.ResponseWriter, r *http.Request) {
 		
+		/* 
+		* get customers from external endpoint
+		*/
 		currentTime := time.Now().Format("2006-01-02")
 
 		urlCustomers := fmt.Sprintf("https://kqxty15mpg.execute-api.us-east-1.amazonaws.com/buyers?date=%s", currentTime)
 
 		resp, err := http.Get(urlCustomers)
 		
-		if err != nil {
-			fmt.Printf("error: %s\n", err.Error())
-		}
+		if err != nil { log.Fatal(err) }
 		
 		defer resp.Body.Close()
 		
-		body, err := ioutil.ReadAll(resp.Body)
+		_, err = ioutil.ReadAll(resp.Body)//body, err := 
 
-		w.Write(body)
+		/*
+		* open connection to local database
+		*/
+		conn, err := grpc.Dial("localhost:9080", grpc.WithInsecure())
+		
+		if err != nil { log.Fatal(err) }
+		
+		defer conn.Close()
+		
+		dgraphClient := dgo.NewDgraphClient(api.NewDgraphClient(conn))
+
+		// set schema
+		op := &api.Operation{
+			Schema: `name: string @index(exact) .`,
+		}
+		
+		ctx := context.Background()
+
+		err = dgraphClient.Alter(ctx, op)
+
+		if err != nil { log.Fatal(err) }
+
+		txn := dgraphClient.NewTxn() // create transaction
+
+		defer txn.Discard(ctx)
+
+		// data to load
+		p := Person{
+			Uid:  "_:alice",
+			Name: "Alice",
+		}
+
+		pb, err := json.Marshal(p) // json encoding of data
+		
+		if err != nil { log.Fatal(err) }
+		
+		// mutation
+		mu := &api.Mutation{
+			SetJson: pb,
+		}
+
+		_, err = txn.Mutate(ctx, mu)
+
+		if err != nil { log.Fatal(err) }
+
+		q := `query all($a: string) {
+			all(func: eq(name, $a)) {
+			  name
+			}
+		}`
+		
+		new_resp, err := txn.QueryWithVars(ctx, q, map[string]string{"$a": "Alice"}) // query
+
+		err = txn.Commit(ctx)
+
+		if err != nil { log.Fatal(err) }
+
+		w.Write(new_resp.Json)
 	})
 
 	http.ListenAndServe(":3000", r)
