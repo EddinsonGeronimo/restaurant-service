90018c2311a088079b1dff286907f83fed92cdf3
EddinsonGeronimo <1072957@est.intec.edu.do>
2021-06-29 14:18:24 -0400

    sync tab behaviour

--- web-client/src/components/Sync.vue
+++ web-client/src/components/Sync.vue
@@ -1,23 +1,31 @@
 <template>
   <v-container>
     <v-row justify="center">
-      <h1>Pick a date</h1>
+      <h1>{{ h1Message }}</h1>
     </v-row>
     <v-row justify="center">
       <v-date-picker 
         v-model="picker" 
         color="#04819E"
-        @click:date="click" 
+        @click:date="click"
+        :disabled="progress"
       >
       </v-date-picker>
     </v-row>
-    <v-row>
+    <v-row justify="center">
+      <v-alert
+        type="error"
+        v-show="showError"
+      >
+      {{ errorMessage }}
+      </v-alert>
       <v-progress-linear
         color="cyan lighten-5"
         indeterminate
         rounded
         height="5"
         v-show="progress"
+        light
         >
       </v-progress-linear>      
     </v-row>
@@ -30,28 +38,36 @@
 
     data: () => ({
         picker: new Date().toISOString().substr(0, 10),
-        progress: false
+        progress: false,
+        showError: false,
+        errorMessage: '',
+        h1Message: 'Pick a date',
     }),
     
     methods: {
       click: async function(date){
         this.progress = true
+        this.showError = false
+        this.errorMessage = ''
+        this.h1Message = 'This might take a few seconds...'
+
         try {
           await this.axios.get(`http://localhost:4000/sync?date=${date}`, 
             {headers: {'Access-Control-Allow-Origin': `http://localhost:9999`}})
             
-          alert(`Data sync successful.`)
+          this.$router.push({path:'/buyers'})
 
         } catch(err) {
+          this.showError = true
           if (err.response) {
-            alert(`Server Error:${err}` )
+            this.errorMessage = `Server Error`          
           } else if (err.request) {
-              alert(`Network Error:${err}`)
+            this.errorMessage = `Network Error`          
           } else {
-              alert(`Client Error:${err}`)
+            this.errorMessage = `Client Error`
           }
-          
         }
+        this.h1Message = ''
         this.progress = false
       }
     }
